55440cc6372375936557fd5d6fa65edb4c9bace1
xiaozheng243 <luzz_1@163.com>
2020-03-22 09:04:00 +0800

    1.简单项目搭建

--- demo/src/main/java/online/yuluo/demo/controller/IndexController.java
+++ demo/src/main/java/online/yuluo/demo/controller/IndexController.java
@@ -1,4 +1,15 @@
 package online.yuluo.demo.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/index")
 public class IndexController {
+
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    public String index() {
+        return "Hello,World";
+    }
 }
