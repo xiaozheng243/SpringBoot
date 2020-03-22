e11b34703c1d2a8434a424e30abde6fafc08c335
xiaozheng243 <luzz_1@163.com>
2020-03-22 09:19:53 +0800

    使用SwaggerUI注解

--- demo/src/main/java/online/yuluo/demo/controller/IndexController.java
+++ demo/src/main/java/online/yuluo/demo/controller/IndexController.java
@@ -1,13 +1,20 @@
 package online.yuluo.demo.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author yuluo
+ */
+@Api(value = "index测试类", description = "描述信息")
 @RestController
 @RequestMapping("/index")
 public class IndexController {
 
+    @ApiOperation(value = "index", notes = "home测试方法") // tags
     @RequestMapping(value = "/home", method = RequestMethod.GET)
     public String index() {
         return "Hello,World";
